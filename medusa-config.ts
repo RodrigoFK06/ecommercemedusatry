d47d8d80dfb4de8c5e009f650ae4693126638e13
Rodrigo  Torres <146679608+RodrigoFK06@users.noreply.github.com>
2025-04-28 18:21:19 -0500

    release1

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,4 +1,4 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig, Modules } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
@@ -7,12 +7,39 @@ export default defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
     http: {
-      authCors: "http://localhost:9000,http://localhost:3000",
-      adminCors: "http://localhost:9000",
-      storeCors: "http://localhost:3000",
+      authCors: process.env.AUTH_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      storeCors: process.env.STORE_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-      compression: { enabled: true }, // <- CORRECTO
+      compression: { enabled: true },
     },
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
   },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL, // lo vas a poner en Railway
+  },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+  ],
 })
